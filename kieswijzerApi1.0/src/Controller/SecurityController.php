688cb41be4bd16a7cb686d44da8c9aaf40840bef
M Sarikaya <31892563+16090209@users.noreply.github.com>
2021-12-07 11:25:52 +0100

    controller aangemaakt

---
+++ kieswijzerApi1.0/src/Controller/SecurityController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Beheerder;
+use App\Entity\Vraag;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+
+    /**
+     * @Route("/security", name="security")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/SecurityController.php',
+        ]);
+    }
+}
