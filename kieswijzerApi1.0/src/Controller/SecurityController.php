883ec8b662f69d6a17f2f9458a0db844433ae5c0
marvinw2000 <Anthonie2000@outlook.com>
2021-12-09 12:07:14 +0100

    update

--- kieswijzerApi1.0/src/Controller/SecurityController.php
+++ kieswijzerApi1.0/src/Controller/SecurityController.php
@@ -18,12 +18,42 @@ class SecurityController extends AbstractController
     /**
      * @Route("/getAllQuestions", name="getAllQuestions")
      */
-    public function getAllQuestions(): Response
+    public function getAllQuestions()
     {
         $em = $this->getDoctrine()->getManager();
         $data = $em->getRepository(Vraag::class)->findAll();
         $response = new Response(json_encode($data));
         $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+    /**
+     * @Route("/createQuestion", name="createQuestion")
+     */
+    public function createQuestion(Request $request)
+    {
+
+        $dataNieuweVraag = json_decode($request->getContent(),
+            true);
+        $nVraag = new Vraag();
+        $nVraag->setVraag($dataNieuweVraag['vraag']);
+        $nVraag->setJuisteAntwoord($dataNieuweVraag['juisteAntwoord']);
+        $nVraag->setPuntenIct($dataNieuweVraag['puntenIct']);
+        $nVraag->setPuntenAenM($dataNieuweVraag['puntenAenM']);
+        $nVraag->setPuntenBenI($dataNieuweVraag['puntenBenI']);
+        $nVraag->setPuntenMei($dataNieuweVraag['puntenMei']);
+        $nVraag->setPuntenTenI($dataNieuweVraag['puntenTenI']);
+        //entity Maneger word aangemaaakt
+        $em =$this->getDoctrine()->getManager();
+        $em->persist($nVraag);
+        $em->flush();
+
+        $response = new JsonResponse(
+            [
+                'addedQuestion' => 'ok',
+            ],
+            JsonResponse::HTTP_CREATED
+        );
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         return $response;
     }
 }
