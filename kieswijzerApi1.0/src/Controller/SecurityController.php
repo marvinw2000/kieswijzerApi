24bb130910d695608ffb6e7219b7049edfb0fab8
marvinw2000 <Anthonie2000@outlook.com>
2021-12-16 11:50:46 +0100

    update

--- kieswijzerApi1.0/src/Controller/SecurityController.php
+++ kieswijzerApi1.0/src/Controller/SecurityController.php
@@ -14,9 +14,8 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class SecurityController extends AbstractController
 {
-
     /**
-     * @Route("/getAllQuestions", name="getAllQuestions" methods={"get"})
+     * @Route("/getAllQuestions", name="getAllQuestions" )
      */
     public function getAllQuestions()
     {
@@ -26,80 +25,5 @@ class SecurityController extends AbstractController
         $response->headers->set('Content-Type', 'application/json');
         return $response;
     }
-
-    /**
-     * @Route("/createQuestion", name="createQuestion" methods={"post"})
-     */
-    public function createQuestion(Request $request)
-    {
-
-        //Entity manager wordt aangeroepen.
-        $em = $this->getDoctrine()->getManager();
-
-        //content wordt van JSON naar PHP object geconverteerd.
-        $data = json_decode($request->getContent(), true);
-
-        //Hier wordt de nieuwe vraag aangemaakt
-        $nieuweVraag = new Vraag();
-        $nieuweVraag->setVraag($data['Vraag']);
-        $nieuweVraag->setJuisteAntwoord($data['JuisteAntwoord']);
-        $nieuweVraag->setPuntenIct($data['puntenIct']);
-        $nieuweVraag->setPuntenAenM()($data['puntenAenM']);
-        $nieuweVraag->setPuntenBenI($data['puntenBenI']);
-        $nieuweVraag->setPuntenMei($data['puntenMei']);
-        $nieuweVraag->setPuntenTenI($data['puntenTenI']);
-
-        //de nieuwe vraag is aangegeven als object.
-        $em->persist($nieuweVraag);
-
-        //de wijzigingen worden uitgevoerd in DB
-        $em->flush();
-
-
-        $response = new JsonResponse(
-            [
-                'addedQuestion' => 'ok',
-            ],
-            JsonResponse::HTTP_CREATED
-        );
-
-        return $response;
-
-    }
-
-    /**
-     * @Route("/updateQuestion", name="updateQuestion" methods={"update"})
-     */
-    public function updateQuestion()
-    {
-
-    }
-
-
-    /**
-     * @Route("/deleteQuestion", name="deleteQuestion" methods={"delete"})
-     */
-    public function deleteQuestion(Request $request, $id)
-    {
-        //Entity manager wordt aangeroepen.
-        $em = $this->getDoctrine()->getManager();
-
-        //data wordt opgehaald uit Vraag entiteit met id
-        $data = $em->getRepository(Vraag::class)->find($id);
-
-        //data wordt verwijderd
-        $em->remove($data);
-
-        //de wijziging wordt gestuurd naar de DB
-        $em->flush();
-
-        //Er wordt een nieuwe response aangemaakt.
-        $response = new Response(json_decode($data));
-        $response->send();
-
-
-    }
-
-
 
 }
